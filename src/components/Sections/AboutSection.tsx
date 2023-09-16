a6cddb1c5b946d827063f9020efc5c9c25449b1f
TanGuanYu <tan_guanyu@hotmail.com>
2023-09-17 01:57:36 +0800

    Update layout and add sections to default page

---
+++ src/components/Sections/AboutSection.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export default function AboutSection() : JSX.Element {
+  return (
+    <Box
+      height='100vh'
+      display='flex'
+      flexDirection='column'
+      justifyContent='center'
+      alignItems='center'
+      bgcolor='white'>
+      <Typography color="black">
+        About.
+      </Typography>
+    </Box>
+  );
+}
---
+++ src/components/Sections/ContactSection.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export default function ContactSection() : JSX.Element {
+  return (
+    <Box
+      height='100vh'
+      display='flex'
+      flexDirection='column'
+      justifyContent='center'
+      alignItems='center'
+      bgcolor='black'>
+      <Typography color="white">
+        Contact.
+      </Typography>
+    </Box>
+  );
+}
